08f81c96ee4e031e053b674d9971234ac3b92edb
ABDULRAHMAN0254 <445050254@std.psau.edu.sa>
2026-03-27 23:12:52 +0300

    set my student ID : 445050254

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -89,7 +89,7 @@ class Process implements Runnable {
                               Colors.RESET + Colors.BRIGHT_GREEN + " finished execution!" + 
                               Colors.RESET);
         }
-        System.out.println();
+        System.out.println(int studintID:445050254;);
     }
     
     // Helper method to create a visual progress bar
